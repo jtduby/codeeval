87cdb57f005eafb79977d8acf2a67cfe7d90ab3c
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-13 23:57:50 -0500

    More work on discount

--- discount/discount.py
+++ discount/discount.py
@@ -1,66 +1,40 @@
 #!/usr/bin/env python
 
 """
-    Prefix Expressions Solution: v0.1
+    Discount Offers Solution: v0.1
 """
 import sys
 
-class Stack(object):
+class Factors(object):
     def __init__(self):
-        self.s = []
-    def push(self, obj):
-        self.s.append(obj)
-    def pop(self):
-        if self.size() > 0:
-            return self.s.pop() 
+        self.f = {}
+    def factors_of(self, n):
+        if (not self.f.has_key(n)):
+            self.find_factors(n)
+        return self.f[n]
+    def find_factors(self, n):
+        print "in find"
+        self.f[n] = set([n])
+        for i in range(2, n):
+            if (n%i==0):
+                self.f[n].add(i)
+    def common_factors(self, x, y):
+        cf = self.factors_of(x) & self.factors_of(y)
+        return cf
+    def has_common_factors(self, x, y):
+        cf = self.common_factors(x, y)
+        if len(cf) > 0:
+            return True
         else:
-            return None
-    def size(self):
-        return len(self.s)
-
-class Operations(object):
-    def __init__(self):
-        self.ops = {"+": (lambda x,y: x + y),
-                    "*": (lambda x,y: x * y),
-                    "/": (lambda x,y: x / y)}
-    def perform(self, expr):
-        op, x, y = expr
-        result = self.ops[op](x, y)
-        return result
+            return False
+        
  
-class Calculator(object):
-    def __init__(self):
-        self.s = Stack()
-        self.o = Operations()
-    def calculate(self, op):
-        y = self.s.pop()
-        x = self.s.pop()
-        result = self.o.perform((op, x, y))
-        self.s.push(result)
-    def evaluate(self, eq_str):
-        eq_list = self.make_eq_list(eq_str)
-        for item in eq_list:
-            if item.isdigit():
-                self.s.push(int(item))
-            else:
-                self.calculate(item)
-        return self.s.pop()
-    def make_eq_list(self, eq_str):
-        tmp_str = eq_str.strip()
-        # print tmp_str
-        eq_list = tmp_str.split()
-        eq_list.reverse()
-        return eq_list
-
 if __name__ == '__main__': 
     test_cases = open(sys.argv[1], 'r')
-    c = Calculator()
 
     for line in test_cases:
-        r = c.evaluate(line)
-        print r        
     test_cases.close()
-
+    f = Factors()
     sys.exit(0)
 
 sys.exit(1)
