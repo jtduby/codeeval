9dfb9bc3b3275e849c5acbb3b4792825a3bd93b4
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-14 19:55:57 -0500

    Still truckin on dicount

--- discount/discount.py
+++ discount/discount.py
@@ -18,6 +18,24 @@ def consonants(s):
     count = len(re.sub(r, '', s))
     return count
 
+def common_factor_score(name, item):
+    """
+    """
+    n = letters(name)
+    i = letters(i)
+    if (has_common_factors(n, i)):
+        return (n * 1.5)
+    return 0.0
+ 
+
+def even_score(name, item):
+    """
+    """
+    if (is_even(item)):
+        v = vowels(name)
+        return (v * 1.5)
+    return 0.0
+
 def find_factors(n):
     """Takes and integer n, returns the set of all factors of n, except 1."""
     f = set([n])
@@ -25,6 +43,13 @@ def find_factors(n):
         if (n%i == 0):
             f.add(i)
     return f
+
+def gen_ss(name, item):
+    """Takes the name and item as strings. Returns the maximum SS."""
+    cf_score = common_factor_score(name, item)
+    e_score = even_score(name, item)
+    o_score = odd_score(name, item)
+    return max(cf_score, e_score, o_score)
 
 def has_common_factors(x, y):
     """Takes two integers, x and y. Returns True if they have common factors,
@@ -37,11 +62,27 @@ def has_common_factors(x, y):
         return True
     return False
 
+def is_even(s):
+    """
+    """
+    let_count = letter(s)
+    if (let_count%2 == 0):
+        return True
+    return False
+
 def letters(s):
     """Takes a string s, returns the integer count of letters in the string.
     """
     count = len(re.sub('[^A-Za-z]', '', s))
     return count
+
+def odd_score(name, item):
+    """
+    """
+    if (not is_even(item)):
+        c = consonants(name)
+        return (c * 1.5)
+    return 0.0
 
 def parse_test_line(s):
     """Takes the string line from the test file. Returns a list of names and a
@@ -51,6 +92,12 @@ def parse_test_line(s):
     name_list = names.split(",")
     items_list = items.split(",")
     return names_list, items_list
+
+def vowels(s):
+    """Takes the string s. Return the integer count of vowels in the string.
+    """
+    count = len(re.sub('[^AEIOUaeiou]', '', s))
+    return count
 
  __name__ == '__main__': 
     test_cases = open(sys.argv[1], 'r')
---
+++ discount/funcs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+
+if __name__ == '__main__':
+    infile = open(sys.argv[1], 'r')
+    for line in infile:
+        if line.startswith('def'):
+            print line
+    infile.close()
