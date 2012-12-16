5094b681673c29c85395152f80cea67724d267ec
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-15 20:35:16 -0500

    Debugging discount

--- discount/discount.py
+++ discount/discount.py
@@ -10,9 +10,13 @@
     Assumptions:
         --> Number of items >= number of customers as a given product may only
             be offered to one customer.
+
+    Version History:
+        0.1:    Brute-force
 """
 import sys
 import re
+import itertools
 
 def consonants(s):
     """Takes a string s, returns the integer count of consonants in the
@@ -26,12 +30,11 @@ def common_factor_score(name, item):
     """
     """
     n = letters(name)
-    i = letters(i)
+    i = letters(item)
     if (has_common_factors(n, i)):
         return (n * 1.5)
     return 0.0
  
-
 def even_score(name, item):
     """
     """
@@ -47,6 +50,47 @@ def find_factors(n):
         if (n%i == 0):
             f.add(i)
     return f
+
+def find_max_scores(names, item_perm, ss_dict):
+    """Please, please don't give me too many inputs. Have mercy. I'm just a 
+    lowly brute-firce solution.
+    """
+    max_ss = 0.0
+    for p in item_perm:
+        temp_ss = 0.0
+        for n in names:
+            for i in p:
+                temp_ss += ss_dict[n][i]
+        if temp_ss > max_ss:
+            max_ss = temp_ss
+    return max_ss
+
+def find_scores(name, items):
+    """Takes a string name, and a list of strings items. Returns a dict
+    containing S scores that are indexed by the items where 
+    SS = ss(name, item) 
+    """
+    s = {}
+    for i in items:
+        s[i] = gen_ss(name, i)
+    return s 
+    
+def gen_permutations(names, items):
+    """Takes a list of string names and a list of string items. Returns
+    len(name) sized permutations of items.
+    """
+    p = itertools.permutations(items, len(names))
+    return p        
+
+def gen_ss_dict(names, items):
+    """Takes a list of string names and a list of strings items. Returns a
+    dict that is indexed by name, and contains dicts of items as returned
+    from find_scores().
+    """
+    s = {}
+    for n in names:
+        s[n] = find_scores(n, items)
+    return s
 
 def gen_ss(name, item):
     """Takes the name and item as strings. Returns the maximum SS."""
@@ -69,7 +113,7 @@ def has_common_factors(x, y):
 def is_even(s):
     """
     """
-    let_count = letter(s)
+    let_count = letters(s)
     if (let_count%2 == 0):
         return True
     return False
@@ -94,8 +138,25 @@ def parse_test_line(s):
     """
     names, items = s.split(";")
     name_list = names.split(",")
-    items_list = items.split(",")
-    return names_list, items_list
+    item_list = items.split(",")
+    return name_list, item_list
+
+def pretty_print(scores):
+    """
+    """
+    for s in scores:
+        print("%0.2f" % s)
+
+def run_test(infile):
+    """
+    """
+    scores = []
+    for line in infile:
+        names, items = parse_test_line(line)
+        ss_dict = gen_ss_dict(names, items)
+        item_perms = gen_permutations(names, items)
+        scores.append(find_max_scores(names, item_perms, ss_dict))
+    return scores
 
 def vowels(s):
     """Takes the string s. Return the integer count of vowels in the string.
@@ -103,12 +164,11 @@ def vowels(s):
     count = len(re.sub('[^AEIOUaeiou]', '', s))
     return count
 
- __name__ == '__main__': 
-    test_cases = open(sys.argv[1], 'r')
-
-    for line in test_cases:
-    test_cases.close()
-    f = Factors()
+if  __name__ == '__main__': 
+    infile = open(sys.argv[1], 'r')
+    scores = run_test(infile)
+    infile.close()
+    pretty_print(scores)
     sys.exit(0)
 
 sys.exit(1)
--- discount/funcs.py
+++ discount/funcs.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     infile = open(sys.argv[1], 'r')
     for line in infile:
         if line.startswith('def'):
-            print line
+            print line.strip()[4:]
     infile.close()
