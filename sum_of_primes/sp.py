16121206e02a07ebcf6cc65850476b2bea94cc88
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-22 22:50:17 -0500

    New version of sum of primes,

--- sum_of_primes/sp.py
+++ sum_of_primes/sp.py
@@ -3,40 +3,39 @@
 """
     Sum of Primes Solution: v0.2
    
-    **Still working on v0.2**
- 
     Version History:
         0.1: Brute force.
         0.2: Implemented memoization and use of computed primes for testing
              the current number.
+        0.3: Modularization of code. **WIP**
 """
 
 import sys
 import math
 
-def prime(n, primes=[]):
+def memo_prime(n, primes):
+    """Takes an integer prime candidate n, and a list of known primes primes.
+    Checks if n is prime based on the primes list. 
+
+    *Warning*   Assumes that there are no primes between the square root of n
+                and n.
+    """
     s = int(math.ceil(math.sqrt(n)))
-    if primes:
-        pass 
-    if n > 3:
-        for i in range(start, s+1):
-            if (n % i == 0):
-                return False
+    for p in primes:
+        if (p > s): break
+        if (n % p == 0): return False
     return True
 
-def sum_list(nlist):
-    s = 0
-    for i in nlist:
-        s = s + i
-    return s
-    
+def n_primes(n):
+
 if __name__ == '__main__':
     primes = []
-    i = 2   
+    i = 2
+    sum = 0
     while len(primes) < 1000:
-        if prime(i):
+        if memo_prime(i, primes):
             primes.append(i)
+            sum += i
         i += 1
-    sum = sum_list(primes)
     print(sum)
     sys.exit(0)
