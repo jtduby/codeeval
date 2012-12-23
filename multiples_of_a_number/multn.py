ea4072a68f101f50a5536b2e26edb98ceb75cc5a
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-23 00:00:07 -0500

    Completed Multiples of a Number. Also fixed exception handling in the template.

--- multiples_of_a_number/multn.py
+++ multiples_of_a_number/multn.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 
 """
-    Solution: v0.1
+    Multiples of a Number Solution: v0.1
 """
 
 import sys
 
+def find_multiple(x, n):
+    while n < x:
+        n = n << 1
+    return n
+
+def parse_line(line):
+    try:
+        tmp = line.strip().split(',')
+        str_x, str_n = tmp
+        x = int(str_x)
+        n = int(str_n)
+    except Exception as e:
+        return e
+    return x, n
+    
 if __name__ == '__main__':
     exit_code = 0
     try: 
         infile = open(sys.argv[1], 'r')
         for line in infile:
+            x, n = parse_line(line)
+            m = find_multiple(x, n)
+            print(m)
     except IOError:
         exit_code = "Unable to open file: " + sys.argv[1]
     except IndexError:
         exit_code = "No arguments given."
+    except ValueError:
+        exit_code = "Invalid input file."
     except:
         exit_code = "Unspecified error."
     finally:
