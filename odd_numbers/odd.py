68264a27188be537d34c490c2c073c0b23dc67ea
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-23 21:35:16 -0500

    Completed odd_numbers.

---
+++ odd_numbers/odd.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""
+    Odd Numbers Solution: v0.1
+"""
+
+import sys
+
+if __name__ == '__main__':
+    for i in range(1, 100):
+        if i%2 > 0:
+            print i
+    sys.exit(0)
