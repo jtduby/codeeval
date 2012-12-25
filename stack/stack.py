6c3849ce0e7fbd23c9c6f2e071578da078fdf459
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-25 15:06:10 -0500

    Still working on Stack Implementation.

--- stack/stack.py
+++ stack/stack.py
@@ -13,32 +13,56 @@ import sys
 class Stack(object):
     def __init__(self):
         self.stack = []
+    def empty(self):
+        if len(self.stack) == 0:
+            return True
+        else:
+            return False
     def push(self, obj):
         self.stack.append(obj)
     def pop(self):
-        try:
-            p = self.pop()
-        except IndexError:
-            p = None
-        finally:
-            return p
+        if (not self.empty()):
+            return self.pop()
+        else: 
+            return None
+        
+class InputFileIncorrect(Exception):
+    def __init__(self, msg=''):
+        self.message = msg
+
+class StackEmpty(BaseException):
+    pass
 
 def parse_line(line):
-    line_list = line.strip().split(',')
-    return line_list
+    if len(line.strip()) > 0:
+        line_list = line.strip().split(' ')
+        return line_list
+    else:
+        raise InputFileIncorrect        
+
+def batch_push(stack, items):
+    for i in items:
+        stack.push(i)
 
 if __name__ == '__main__':
     exit_code = 0
     try: 
         infile = open(sys.argv[1], 'r')
+        s = Stack()
         for line in infile:
+            items = parse_line(line)
+            batch_push(s, items)
+            alt = False
             
+            
+                
+                        
     except IOError:
         exit_code = "Unable to open file: " + sys.argv[1]
     except IndexError:
         exit_code = "No arguments given."
-    except:
-        exit_code = "Unspecified error."
+    except InputFileIncorrect:
+        exit_code = "Input file not properly formatted."
     finally:
         try:
             infile.close()
