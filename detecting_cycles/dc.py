61712d8971373bb582745bb08c756e82b3fcd238
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-31 14:27:26 -0500

    Fixing licensing.

--- detecting_cycles/dc.py
+++ detecting_cycles/dc.py
@@ -7,7 +7,6 @@
 import sys
 
 if __name__ == '__main__':
-    exit_code = 0
     try: 
         infile = open(sys.argv[1], 'r')
         for line in infile:
@@ -15,16 +14,6 @@ if __name__ == '__main__':
             seq = line.strip().split(' ')
             for i in range(0, len(seq)):
                pass 
-    except IOError:
-        exit_code = "Unable to open file: " + sys.argv[1]
-    except IndexError:
-        exit_code = "No arguments given."
-    except:
-        exit_code = "Unspecified error."
     finally:
-        try:
-            infile.close()
-        except NameError:
-            pass            # Look up printing to stderr, then do something
-                            # better here
-    sys.exit(exit_code)
+        infile.close()
+    sys.exit(0)
--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -11,40 +11,29 @@ import sys
 
 class Fib(object):
     def __init__(self):
-        self.fibs = [0, 1]
-        self.nth_fib = 1
+        self.fibs = {0:0, 1:1}
     def fib(self, n):
-        if (self.nth_fib >= n):
-            return self.fibs[n]
-        else:
-            self.find_fib(n)
-            self.nth_fib = n
-            return self.fibs[-1]
-    def find_fib(self, n):
-        while (self.nth_fib < n):
-            self.fibs.append(self.fibs[-1] + self.fibs[-2])
-            self.nth_fib += 1
+        try:
+            f = self.fibs[n]
+        except:
+            self._find_fib(n)
+            f = self.fibs[n]
+        return f
+    def _find_fib(self, n):
+        for i in range(self._last_computed_fib()+1, n+1):
+            self.fibs[i] = self.fibs[i-1] + self.fibs[i-2]
+    def _last_computed_fib(self):
+        computed_fibs = self.fibs.keys()
+        computed_fibs.sort()
+        return computed_fibs[-1]
 
 if __name__ == '__main__':
-    exit_code = 0
     try: 
         infile = open(sys.argv[1], 'r')
         fib = Fib()
         for line in infile:
             n = int(line.strip())
             print fib.fib(n)
-    except IOError:
-        exit_code = "Unable to open file: " + sys.argv[1]
-    except IndexError:
-        exit_code = "No arguments given."
-    except ValueError:
-        exit_code = "Invalid input format."
-    except:
-        exit_code = "Unspecified error."
     finally:
-        try:
-            infile.close()
-        except NameError:
-            pass            # Look up printing to stderr, then do something
-                            # better here
-    sys.exit(exit_code)
+        infile.close()
+    sys.exit(0)
