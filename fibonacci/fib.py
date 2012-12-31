72da45d0e59b7d54b8fc7cc1dc121c778b41fb90
Jamie Thomas Duby <jtduby@gmail.com>
2012-12-31 16:02:02 -0500

    Refactoring and cleaning.

--- fibonacci/fib.py
+++ fibonacci/fib.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 
 """
-    Fibonacci Solution: v0.1
+    Fibonacci Solution: v0.3
     
-    Couldn't bring myself to calculate the fib each time. Created an object
-    to memoize already calculated values.
+    Version History:
+        0.2: Couldn't bring myself to calculate the fib each time. Created an
+             object to memoize already calculated values.
+        0.3  Refactored and cleaned up code.
+    
+    =================================
+    Copyright 2012, Jamie Thomas Duby
+    =================================
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+  
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
 import sys
