f8c42125c45b92e711de7ea9f58e10e8656354df
Jamie Thomas Duby <jtduby@gmail.com>
2013-06-25 14:49:00 -0400

    Finished even_numbers

--- even_numbers/even_numbers.py
+++ even_numbers/even_numbers.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
 """
-    Solution: v0.1
+    Even Numbers Solution: v0.1
+
+    Takes, as an argument, the path to a file with an integer n on
+    each line that is between 0 and 5000 exclusively. Prints to stdout:
+    1 if n is even, 0 if n is odd. 
+
+    =================================
+    Copyright 2013, Jamie Thomas Duby
+    =================================
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+  
+    http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
 import sys
@@ -10,6 +30,7 @@ if __name__ == '__main__':
     try: 
         infile = open(sys.argv[1], 'r')
         for line in infile:
+            print((int(line.strip()) % 2) ^ 1)
     finally:
         infile.close()
     sys.exit(0)
